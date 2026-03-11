17687326ee27807e842537c44636deafb74ef418
Henrique Junqueira <henriquecamposj@gmail.com>
2026-03-10 22:12:07 -0300

    indy-schedule 0.1.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Casks/indy-schedule.rb
+++ Casks/indy-schedule.rb
@@ -1,8 +1,8 @@
 cask "indy-schedule" do
-  version "0.1.1"
-  sha256 "29ac19c4ab8977c88be9f531ee535cb7a1ad0adf14156b289d54ee14b056c909"
+  version "0.1.2"
+  sha256 "7eb5b05c7880dfe046c8c09380046563e6041330b052b4e826963f8b45a961fe"
 
-  url "https://github.com/henriquecdb/indy-schedule/releases/download/v0.1.1/indy-schedule.zip"
+  url "https://github.com/henriquecdb/indy-schedule/releases/download/v0.1.2/indy-schedule.zip"
   name "Indy Schedule"
   desc "Um aplicativo macOS minimalista para acompanhar o calendário da temporada 2026 da IndyCar Series."
   homepage "https://github.com/henriquecdb/indy-schedule"
